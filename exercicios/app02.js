fb27143534c19c54c8e7cec14c55c007413d577c
NiczinXD <nicolasxpinheiro@gmail.com>
2024-11-10 10:35:09 -0300

    Manipulando objetos/desafios2

--- exercicios/app02.js
+++ exercicios/app02.js
@@ -128,9 +128,6 @@ const array = [1, 2, 3, 4, 5]
 console.log(calculadora.calcularMedia(array));
 pare()
 
-// estaAprovado: function(mediaBase){
-//   return this.media >= mediaBase ? true : false
-// }
 // 5 #####################################################################################################################
 console.log("5 - Crie um objeto chamado contaBancaria com as seguintes propriedades: titular: uma string representando o titular da conta. saldo: um número representando o saldo da conta. depositar: uma função que aceita um valor como parâmetro e adiciona esse valor ao saldo da conta. Utilize this para acessar a propriedade saldo. sacar: uma função que aceita um valor como parâmetro e subtrai esse valor do saldo da conta. Utilize this para acessar a propriedade saldo. Certifique-se de verificar se há saldo suficiente antes de efetuar o saque. Crie um objeto chamado cliente que representa um cliente com uma conta bancária. O objeto deve ter as seguintes propriedades: nome: uma string representando o nome do cliente. conta: uma referência à conta bancária associada a esse cliente (objeto criado anteriormente). Crie uma função chamada mostrarSaldo que aceita o objeto cliente como parâmetro e imprime no console o nome do cliente e o saldo da sua conta utilizando this para acessar as propriedades do objeto. Realize operações de depósito e saque na conta bancária do cliente usando as funções do objeto contaBancaria e, em seguida, chame a função para exibir as informações atualizadas no console.\n".cyan);
 const contaBancaria = {
